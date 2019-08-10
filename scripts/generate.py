c0e3a57b56187f5feaaf592ed463bff640d46c2a
Geordon Worley <vadixidav@gmail.com>
2019-08-10 19:12:48 -0400

    update generate.py to allow float and binary descriptors

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,14 +1,21 @@
-"""generate
+"""
+generate
 
-Usage: generate.py <image>...
+Usage:
+  generate.py <descriptor> <image>...
 
 Options:
   -h --help         Show this screen.
   --version         Show the version.
 
+<descriptor> can be one of the following:
+  * akaze (outputs 61 bytes)
+  * kaze (outputs 64 32-bit floats)
 """
 
 import sys
+import struct
+import itertools
 import cv2
 from progress.bar import Bar
 from docopt import docopt
@@ -16,13 +23,28 @@ from docopt import docopt
 if __name__ == '__main__':
     ARGUMENTS = docopt(__doc__, options_first=True, version='1.0.0')
     IMAGES = ARGUMENTS["<image>"]
+    DESCRIPTOR = ARGUMENTS["<descriptor>"]
     BAR = Bar('Progress', max=len(IMAGES))
+
+    if DESCRIPTOR == "akaze":
+        DESCRIPTOR_OBJECT = cv2.AKAZE.create()
+        BINARY = True
+    elif DESCRIPTOR == "kaze":
+        DESCRIPTOR_OBJECT = cv2.KAZE.create()
+        BINARY = False
+    else:
+        sys.exit("unknown descriptor {}".format(DESCRIPTOR))
 
     for filename in IMAGES:
         image = cv2.imread(filename)
-        akaze = cv2.AKAZE.create()
-        keypoints, descriptors = akaze.detectAndCompute(image, None)
+        keypoints, descriptors = DESCRIPTOR_OBJECT.detectAndCompute(
+            image, None)
+
         for descriptor in descriptors:
-            sys.stdout.buffer.write(descriptor)
+            if BINARY:
+                sys.stdout.buffer.write(descriptor)
+            else:
+                sys.stdout.buffer.write(bytes(itertools.chain.from_iterable(
+                    map(lambda f: struct.pack('f', f), descriptor))))
         BAR.next()
     BAR.finish()
