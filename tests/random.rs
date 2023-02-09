b2f704c14e6116f2f18d6b296dfc3628cb9add25
yk-goblin <side.ula@outlook.com>
2023-02-09 10:40:16 +0900

    Impl random test.

--- tests/random.rs
+++ tests/random.rs
@@ -6,32 +6,48 @@ use rand_pcg::Pcg64;
 
 #[test]
 fn random() {
-    let mut searcher = Searcher::default();
-    let mut hnsw = Hnsw::<_, Vec<f32>, Pcg64, 12, 24>::new(SimpleEuclidean);
-    let features = [
-        vec![0.0, 0.0, 0.0, 1.0],
-        vec![0.0, 0.0, 1.0, 0.0],
-        vec![0.0, 1.0, 0.0, 0.0],
-        vec![1.0, 0.0, 0.0, 0.0],
-        vec![0.0, 0.0, 1.0, 1.0],
-        vec![0.0, 1.0, 1.0, 0.0],
-        vec![1.0, 1.0, 0.0, 0.0],
-        vec![1.0, 0.0, 0.0, 1.0],
-    ];
+    const SAMPLES: usize = 100;
 
-    let query = vec![0.0, 0.0, 0.0, 1.0];
+    let mut searcher = Searcher::default();
+
+    let (features, query): (Vec<_>, _) = {
+        use rand::Rng as _;
+        let mut rng = rand::thread_rng();
+
+        let mut query = [0f32; 4];
+        rng.fill(&mut query);
+
+        (
+            std::iter::repeat_with(|| {
+                let mut feature = [0f32; 4];
+                rng.fill(&mut feature);
+
+                feature.to_vec()
+            })
+            .take(SAMPLES)
+            .collect(),
+            query.to_vec(),
+        )
+    };
+
+    let mut hnsw = Hnsw::<_, Vec<f32>, Pcg64, 12, 24>::new(SimpleEuclidean);
 
     for feature in features.clone() {
         hnsw.insert(feature, &mut searcher);
     }
 
-    let mut neighbors = [Neighbor {
-        index: 0,
-        distance: EncodableFloat { value: f32::MAX },
-    }; 8];
-    hnsw.nearest(&query, 24, &mut searcher, &mut neighbors);
+    let neighbors = {
+        let mut neighbors = [Neighbor {
+            index: 0,
+            distance: EncodableFloat { value: f32::MAX },
+        }; SAMPLES];
 
-    let mut features: Vec<_> = {
+        hnsw.nearest(&query, 24, &mut searcher, &mut neighbors);
+
+        neighbors
+    };
+
+    let features: Vec<_> = {
         use hnsw::metric::Metric as _;
 
         let euclidean_distance = SimpleEuclidean;
@@ -46,14 +62,34 @@ fn random() {
             .collect()
     };
 
-    neighbors.sort_by(|a, b| {
-        a.distance
-            .value
-            .partial_cmp(&b.distance.value)
-            .unwrap()
-            .then(a.index.cmp(&b.index))
-    });
-    features.sort_by(|a, b| a.distance.value.partial_cmp(&b.distance.value).unwrap());
+    // neighbors.sort_by(|a, b| {
+    //     a.distance
+    //         .value
+    //         .partial_cmp(&b.distance.value)
+    //         .unwrap()
+    //         .then(a.index.cmp(&b.index))
+    // });
+    // features.sort_by(|a, b| a.distance.value.partial_cmp(&b.distance.value).unwrap());
 
-    assert_eq!(neighbors, features[..]);
+    println!(
+        "{} {}",
+        neighbors
+            .iter()
+            .filter(|neighbor| features.contains(neighbor))
+            .count(),
+        neighbors
+            .iter()
+            .filter(|neighbor| features.contains(neighbor))
+            .count() as f32
+            / features.len() as f32
+    );
+
+    assert!(
+        neighbors
+            .iter()
+            .filter(|neighbor| features.contains(neighbor))
+            .count() as f32
+            / features.len() as f32
+            >= 0.9
+    );
 }
